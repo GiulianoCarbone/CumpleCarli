15320d5568f2443527d3c0d3d2b65e5b3e8a6c6d
Giuliano Carbone <benjacarbone@gmail.com>
2021-12-28 19:37:58 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
       hour = minute * 60,
       day = hour * 24;
   
-    let birthday = "dec 26, 2021 00:00:00",
+    let birthday = "dec 26, 2022 00:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function () {
         let now = new Date().getTime(),
